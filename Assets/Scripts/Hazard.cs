ca76629105d0ae5b0360f5903db25034ad8448a3
heyitsamanie <42983780+heyitsamanie@users.noreply.github.com>
2018-10-21 19:15:48 -0500

    Oh Boy: Level 2, Game Over Scene
    So much stuff, oh my goodness I need to get better at remembering to save on here more

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hazard : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player entered hazard.");
+            CharacterController player = collision.GetComponent<CharacterController>();
+            SceneManager.LoadScene("GameOver");
+
+        }
+
+        else
+        {
+            Debug.Log("Something other than the player entered the hazard.");
+        }
+
+        Debug.Log("Something entered the hazard");
+    }
+
+}
